bf484a325b67cf6f078fca2a8f503f91ee15e989
Deepak Ramani <94072512+deepakxramani@users.noreply.github.com>
2024-01-14 12:51:19 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+    
+    console.log("Hitesh")
+
+
+
+
+let name = "hitesh" // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+let state; // undefined
+let nothing = null; // output: null
+console.log(nothing)
+
+
+// Primitive Data Types:
+
+// number => 2 to power 53 (range of number)
+// bigInt for bigger number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "hitesh"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
