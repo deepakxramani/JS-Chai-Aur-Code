61457e6338ac21148129e8b088b16f20d92aba0f
Deepak Ramani <94072512+deepakxramani@users.noreply.github.com>
2024-01-14 12:55:59 +0000

    new update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// let accountState; // undefined
+let accountState = 11;
+
+
+// accountId = 2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword = "1212121212"
+accountCity = "Bengaluru"
+
+
+
+console.log(accountId);
+
+/*
+Prefer not to use (var) because of block and functional scope.
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
